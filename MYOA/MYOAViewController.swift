985929ac50d13b43d0b45ffd176bf790fff28a8c
CURTIS MCMILLIN <curtismcmillin@CURTISs-iMac.local>
2016-07-10 10:59:41 -0500

    complete MYOA

--- MYOA/MYOAViewController.swift
+++ MYOA/MYOAViewController.swift
@@ -8,16 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MYOAViewController : UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title:"Start Over", style: .Plain, target: self, action: "startOver")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func startOver(){
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
     }
 
 
